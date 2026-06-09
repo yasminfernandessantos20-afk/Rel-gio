d513edd0fcc4a90d685b0fd49dfb021c18de54fa
yasminfernandessantos20-afk <yasmin.fernandes.santos20@escola.pr.gov.br>
2026-06-09 11:50:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//O JavaScript é uma linguagem WEB
+
+//Variáveis - Valores armazenados na memória do computador
+
+//Funções - Blocos de código 
+
+function carregar(){
+    alert("Bem vindo ao JavaScript")
+}
