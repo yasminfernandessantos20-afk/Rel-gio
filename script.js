47626de08c2f3f1255640b270d7486bcb4b70ea4
yasminfernandessantos20-afk <yasmin.fernandes.santos20@escola.pr.gov.br>
2026-06-16 11:23:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,17 @@
 //Funções - Blocos de código 
 
 function carregar(){
-    alert("Bem vindo ao JavaScript")
+    //alert("Bem vindo ao JavaScript")
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+
+    var data = new Date()
+    var hora = data.getHours()
+    var minutos = data.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora}:${minutos}Am`
+
+
+
+
 }
