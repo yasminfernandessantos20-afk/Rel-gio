56fefec97545997d776316e904ad33f4588a0721
yasminfernandessantos20-afk <yasmin.fernandes.santos20@escola.pr.gov.br>
2026-06-16 12:00:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,17 @@ function carregar(){
 
     msg.innerHTML = `Agora são ${hora}:${minutos}Am`
 
-
-
+    if(hora >= 0 && hora <12){
+        img.src = 'manha.png'
+        document.body.style.background ='#fcf6bd'
+    }
+    else if(hora >= 12 && hora <18){
+        img.src = 'tarde.png'
+        document.body.style.background ='#90e0ef'
+    }
+    else{
+        img.src = 'noite.png'
+        document.body.style.background ='#003566'
+    }
 
 }
